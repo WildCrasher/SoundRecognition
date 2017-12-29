adfe7ceef72962fb6eca6c2208d99e802983e0d0
WildCrasher <dzikigeneral1996@o2.pl>
2017-12-29 16:50:03 +0100

    korelacja

--- sr.py
+++ sr.py
@@ -14,7 +14,7 @@ sound_time = n/w
 t = linspace(0, sound_time, n, endpoint=False)
 #t = arange(0, sound_time, 1/w)
 
-subplot(211)
+subplot(311)
 plot(t, signal, '*')
 xlabel("Probki")
 ylabel("Signal[t]")
@@ -23,10 +23,17 @@ signal1 = fft(signal)*2/n
 signal1 = abs(signal1)
 freqs = linspace(0, w, n, endpoint=False)
 
-subplot(212)
+subplot(312)
 plot(freqs, signal1, '-*')
 #stem(freqs, signal1, '-*')
 xlabel("w [Hz]")
 ylabel('fft(signal)')
+
+rxx_lags = xcorr(signal, signal)
+rxx = rxx_lags[0]
+lags = rxx_lags[1]
+
+subplot(313)
+plot(lags, rxx, '.')
 
 show()
