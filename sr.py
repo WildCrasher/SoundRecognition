967148b9d898d41e54acfe96c3c963d03390e730
WildCrasher <dzikigeneral1996@o2.pl>
2017-12-30 21:03:30 +0100

    naprawione correlate , zostalo tylko wytestowanie drugiego szczytu, prawie zakodowane

--- sr.py
+++ sr.py
@@ -23,34 +23,19 @@ n = len(signal)
 
 sound_time = n/w
 t = linspace(0, sound_time, n, endpoint=False)
-#t = arange(0, sound_time, 1/w)
-
-#subplot(311)
-#plot(t, signal, '*')
-#xlabel("Probki")
-#ylabel("Signal[t]")
 
 signal1 = fft(signal)*2/n
 signal1 = abs(signal1)
-freqs = linspace(0, w, n, endpoint=False)
 
-#subplot(312)
-#plot(freqs, signal1, '-*')
-#xlabel("w [Hz]")
-#ylabel('fft(signal)')
-
-#[rxx, lags] = xcorr(signal, signal, maxlags = len(signal))
-
-#rxx_lags = xcorr(signal, signal, maxlags=2 )
-#rxx = rxx_lags[0]
-#lags = rxx_lags[1]
-
-
-rxx = np.correlate(signal, signal,"full")
+rxx = np.correlate(signal/10000, signal/10000,"valid")
 #lags = linspace(-len(signal) + 1, len(signal) - 1, len(signal)*2 - 1 )
-
+rxx = np.correlate(signal/10000, signal/10000, "full")
 
 first_pick_lock = len(signal) + 1
+
+print(len(rxx))
+print(first_pick_lock)
+print(index_of_max(rxx))
 
 half_min = 15
 
@@ -68,22 +53,5 @@ print(first_pick_lock)
 print(rxx[first_pick_lock])
 print(index_of_max(rxx))
 print(max(rxx))
-'''
-print('MAX:')
-print(max(rxx))
 
-print('index max')
-print(index_of_max(rxx))
 
-print("srodek")
-print(rxx[len(signal) + 1])
-
-print("index srodek")
-print(len(signal) + 1)
-
-print("Last index")
-print(len(rxx) - 1)
-'''
-#subplot(313)
-
-#show()
